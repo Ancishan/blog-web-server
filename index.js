f6e9ab065a2be737fa970af45a97b8fb2c855988
Ancishan <shantodev027@gmail.com>
2024-05-11 17:57:13 +0600

    [DONE] add blog fucntionality

--- index.js
+++ index.js
@@ -2,51 +2,57 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 5000
 
 const app = express()
 const corsOptions = {
-    origin: ['http://localhost:5173'],
-    credentials: true,
-    optionSuccessStatus: 200,
+  origin: ['http://localhost:5173','http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions))
 app.use(express.json())
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nhcslav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-      const jobCollection = client.db('soloSphere').collection('jobs')
-      const bidsCollection = client.db('soloSphere').collection('bids')
-
-
-    
-
-
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-   
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  run().catch(console.dir);
+});
+async function run() {
+  try {
+    const blogCollection = client.db('blog').collection('blogs')
+    // const bidsCollection = client.db('soloSphere').collection('bids')
+
+    // save a job data in db
+    app.post('/blog', async (req, res) => {
+      const BlogData = req.body
+      const result = await blogCollection.insertOne(BlogData)
+      res.send(result)
+    })
 
 
-app.get('/', (req, res) =>{
-    res.send('ruuning soloSphere Server')
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('ruuning soloSphere Server')
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
